5c802e807f302ac3c635af99523be697ed1b1ea5
Talison <talison.kennedy@academico.ifpb.edu.br>
2019-12-17 20:10:34 -0300

    Add files via upload

---
+++ setup2.sh
@@ -0,0 +1,31 @@
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+mkdir /home/$USER/.fonts
+
+cd /home/$USER/.fonts
+
+wget -c https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf
+
+wget -c https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf
+
+wget -c https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf
+
+wget -c https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf
+
+
+
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
+
+sudo rm /home/$USER/.zshrc
+
+sudo echo \
+"export ZSH=\"/home/$USER/.oh-my-zsh\"
+
+ZSH_THEME=\"powerlevel10k/powerlevel10k\"
+
+plugins=(git zsh-autosuggestions)
+
+source \$ZSH/oh-my-zsh.sh
+
+export PATH=\$PATH:/home/$USER/idea/bin:/home/$USER/clion/bin:/home/$USER/pycharm/bin
+" >> /home/$USER/.zshrc
