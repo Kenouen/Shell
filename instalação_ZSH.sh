0d430eef1f297d9ced7deef7361e599d54670cec
Talison <talisonkennedy1@gmail.com>
2020-02-28 20:17:53 -0300

    middle point

--- instalação_ZSH.sh
+++ instalação_ZSH.sh
@@ -3,9 +3,13 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+#plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+
+#Fonts base
 mkdir /home/$USER/.fonts
 
 cd /home/$USER/.fonts
@@ -19,17 +23,18 @@ wget -c https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts
 wget -c https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf
 
 
+# instalando o p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
 
-sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
+#atualizando o .zshrc
+rm /home/$USER/.zshrc
 
-sudo rm /home/$USER/.zshrc
-
-sudo echo \
+echo \
 "export ZSH=\"/home/$USER/.oh-my-zsh\"
 
 ZSH_THEME=\"powerlevel10k/powerlevel10k\"
 
-plugins=(git zsh-autosuggestions)
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 source \$ZSH/oh-my-zsh.sh
 
