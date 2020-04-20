dcb4752092c0f59ff6bfe2372b4094bd757cdb75
talison <kenouen@hotmail.com>
2020-04-20 16:56:59 -0300

    pequenas alterações e adição do laço para controle de instalação

--- Aplicativos_padrao.sh
+++ Aplicativos_padrao.sh
@@ -4,7 +4,10 @@ cd /tmp/temp
 
 #instalação de programas padrão
 
-sudo apt-get install wget transmission zsh git curl openjdk-11-jdk-headless openjdk-11-source openjdk-11-dbg openjdk-11-doc openjfx maven sl logisim vsftpd g++ python3-pip inkscape x11-apps
+for i in logisin code wget transmission zsh git curl openjdk-11-jdk-headless openjdk-11-source openjdk-11-dbg openjdk-11-doc openjfx maven sl logisim vsftpd g++ python3-pip inkscape x11-apps
+do
+    sudo apt-get install $i
+done
 
 
 #Download e instalação das ides
@@ -13,11 +16,9 @@ wget -c https://download-cf.jetbrains.com/python/pycharm-professional-2020.1.tar
 
 wget -c https://download-cf.jetbrains.com/idea/ideaIU-2020.1.tar.gz
 
-wget -c https://download-cf.jetbrains.com/cpp/CLion-2020.1.1.tar.gz
+wget -c https://download-cf.jetbrains.com/cpp/CLion-2020.1.tar.gz
 
 wget -c https://download.virtualbox.org/virtualbox/6.1.2/virtualbox-6.1_6.1.2-135662~Ubuntu~bionic_amd64.deb
-
-wget -c https://az764295.vo.msecnd.net/stable/26076a4de974ead31f97692a0d32f90d735645c0/code_1.41.1-1576681836_amd64.deb
 
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
@@ -33,7 +34,6 @@ sudo dpkg -i virtualbox*
 
 sudo dpkg -i google*
 
-sudo dpkg -i code*
 
 
 #remoção da pasta temporaria
@@ -52,6 +52,54 @@ mv pycharm-* pycharm
 cd /home/$USER
 
 sudo chown -R $USER clion idea pycharm 
+
+
+#Icones do Desktop
+
+cd /home/$USER/Desktop
+
+touch Intellij.desktop Clion.desktop Pycharm.desktop
+
+#intellij
+echo \
+"[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Icon=/home/$USER/idea/bin/idea.png
+Exec=sh /home/$USER/idea/bin/idea.sh
+Name= Intellij
+" >> Intellij.desktop
+
+chmod a+x Intellij.desktop
+
+#Clion
+echo \
+"[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Icon=/home/$USER/clion/bin/clion.png
+Exec=sh /home/$USER/clion/bin/clion.sh
+Name= Clion
+" >> Clion.desktop
+
+chmod a+x Clion.desktop
+
+#Pycharm
+echo \
+"[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Icon=/home/$USER/pycharm/bin/pycharm.png
+Exec=sh /home/$USER/pycharm/bin/pycharm.sh
+Name= Pycharm
+" >> Pycharm.desktop
+
+chmod a+x Pycharm.desktop
+
+
 #Configuração do vsftpd
 
 #Criação do diretório padrão
@@ -87,4 +135,5 @@ rsa_cert_file=/etc/ssl/private/vsftpd.pem
 #instalação do synapse
 
 sudo add-apt-repository ppa:synapse-core/ppa
-sudo apt-get update && sudo apt-get install synapse
+sudo apt-get update
+sudo apt-get install synapse
